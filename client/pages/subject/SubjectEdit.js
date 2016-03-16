905b49b5a5326d2bc736bd105bb99815ff6cd6d5
nscarcella <Nicolas Scarcella>
2016-03-16 19:46:16 -0300

    replaced either calls with parameterized template helpers

--- client/pages/subject/SubjectEdit.js
+++ client/pages/subject/SubjectEdit.js
@@ -9,13 +9,8 @@ Template.SubjectEdit.onCreated(function(){
 	})
 })
 
-const either = (context,edit,create) => context._id ? edit : create
-
 Template.SubjectEdit.helpers({
-	title(){ return either(this.target, 'Edit Subject', 'Create Subject') },
-	doc(){ return either(this.target, this.target, null) },
-	formType(){ return either(this.target, 'transactional-update', 'transactional-insert')},
-	submitText() { return either(this.target, 'Update', 'Create') }
+	either(edit,create){return this.target._id ? edit : create },
 })
 
 Template.SubjectEdit.events({
